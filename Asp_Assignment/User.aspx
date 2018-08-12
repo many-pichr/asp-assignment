d1628edddca127dd161a4c3f7a370d34ba86fdc0
sreymompnc <sreymom.mann@gmail.com>
2018-08-12 11:39:51 +0700

    Master branch

---
+++ Asp_Assignment/User.aspx
@@ -0,0 +1,286 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="User.aspx.cs" Inherits="Asp_Assignment.User" %>
+
+ 
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head runat="server">
+
+    <title>Gridview Basics example.</title>
+
+    <style type="text/css">
+
+        .gv
+
+        {
+
+            font-family: Arial;
+
+            margin-top: 30px;
+
+            font-size: 14px;
+
+        }
+
+        .gv th
+
+        {
+
+            background-color: #5D7B9D;
+
+            font-weight: bold;
+
+            color: #fff;
+
+            padding: 2px 10px;
+
+        }
+
+        .gv td
+
+        {
+
+            padding: 2px 10px;
+
+        }
+
+        input[type="submit"]
+
+        {
+
+            margin: 2px 10px;
+
+            padding: 2px 20px;
+
+            background-color: #5D7B9D;
+
+            border-radius: 10px;
+
+            border: solid 1px #000;
+
+            cursor: pointer;
+
+            color: #fff;
+
+        }
+
+        input[type="submit"]:hover
+
+        {
+
+            background-color: orange;
+
+        }
+
+        .auto-style1 {
+            height: 23px;
+        }
+        .auto-style2 {
+            height: 26px;
+        }
+        #Select1 {
+            width: 256px;
+        }
+
+    </style>
+
+</head>
+
+<body>
+
+    <form id="form1" runat="server">
+
+    <div>
+
+        <table align="center" style="position: relative; top: 20px;">
+
+            <tr>
+
+                <td>
+
+                    <table align="center">
+
+                        <tr>
+
+                            <td>
+
+                                User Name :
+
+                            </td>
+
+                            <td>
+
+                                <asp:TextBox ID="username" runat="server" MaxLength="50" Width="250px"></asp:TextBox>
+
+                            </td>
+
+                        </tr>
+
+                        <tr>
+
+                            <td>
+
+                               Email :
+
+                            </td>
+
+                            <td>
+
+                                <asp:TextBox ID="email" runat="server" MaxLength="10" Width="250px"></asp:TextBox>
+
+                            </td>
+
+                        </tr>
+
+                        <tr>
+
+                            <td class="auto-style2">
+
+                                Password :
+
+                            </td>
+
+                            <td class="auto-style2">
+
+                                <asp:TextBox ID="password" runat="server" MaxLength="200" Width="250px" OnTextChanged="password_TextChanged"></asp:TextBox>
+
+                            </td>
+
+                        </tr>
+                        <tr>
+
+                            <td class="auto-style1">
+
+                                User Role :
+
+                            </td>
+
+                            <td class="auto-style1">
+
+                                <asp:DropDownList ID="role" runat="server">
+                                </asp:DropDownList>
+
+                            </td>
+
+                        </tr>
+
+                        <tr>
+
+                            <td colspan="2" align="center">
+
+                                <asp:Button ID="btnSave" runat="server" Text="Save" OnClick="btnSave_Click" />
+
+                                <asp:Button ID="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click"
+
+                                    Visible="false" />
+
+                                <asp:Button ID="btnClear" runat="server" Text="Clear" OnClick="btnClear_Click" />
+
+                            </td>
+
+                        </tr>
+
+                    </table>
+
+                </td>
+
+            </tr>
+
+            <tr>
+
+                <td align="center">
+
+                    <br />
+
+                    <asp:Label ID="lblMessage" runat="server" EnableViewState="false" ForeColor="Blue"></asp:Label>
+
+                </td>
+
+            </tr>
+
+            <tr>
+
+                <td>
+
+                    <asp:GridView ID="gvDepartments" runat="server" AutoGenerateColumns="False" ShowHeaderWhenEmpty="True"
+
+                        EmptyDataText="No Records Found" GridLines="both" CssClass="gv" EmptyDataRowStyle-ForeColor="Red" OnSelectedIndexChanged="gvDepartments_SelectedIndexChanged">
+
+                        <Columns>
+
+                            <asp:TemplateField HeaderText="User Name">
+
+                                <ItemTemplate>
+
+                                    <asp:Label ID="lblUserName" runat="server" Text='<%#Eval("u_username") %>'></asp:Label>
+
+                                </ItemTemplate>
+
+                            </asp:TemplateField>
+
+                            <asp:TemplateField HeaderText="Email">
+
+                                <ItemTemplate>
+
+                                    <asp:Label ID="lblEmail" runat="server" Text='<%#Eval("email") %>'></asp:Label>
+
+                                </ItemTemplate>
+
+                            </asp:TemplateField>
+
+                            <asp:TemplateField HeaderText="Password">
+
+                                <ItemTemplate>
+
+                                    <asp:Label ID="lblPassword" runat="server" Text='<%#Eval("password") %>'></asp:Label>
+
+                                </ItemTemplate>
+
+                            </asp:TemplateField>
+                             <asp:TemplateField HeaderText="User Role">
+
+                                <ItemTemplate>
+
+                                    <asp:Label ID="lblRole" runat="server" Text='<%#Eval("ur_name") %>'></asp:Label>
+
+                                </ItemTemplate>
+
+                            </asp:TemplateField>
+
+                            <asp:TemplateField HeaderText="Action">
+
+                                <ItemTemplate>
+
+                                    <asp:Button ID="btnEdit" runat="server" Text="Edit" OnClick="btnEdit_Click" />
+
+                                    <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClientClick="return confirm('Are you sure? want to delete the department.');"
+
+                                        OnClick="btnDelete_Click" />
+
+                                    <asp:Label ID="lblUserID" runat="server" Text='<%#Eval("u_id") %>' Visible="false"></asp:Label>
+
+                                </ItemTemplate>
+
+                            </asp:TemplateField>
+
+                        </Columns>
+
+                    </asp:GridView>
+
+                </td>
+
+            </tr>
+
+        </table>
+
+        <input type="hidden" runat="server" id="hidUserID" />
+
+    </div>
+
+    </form>
+
+</body>
+
+</html>
---
+++ Asp_Assignment/User.aspx.cs
@@ -0,0 +1,398 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Web;
+
+using System.Web.UI;
+
+using System.Web.UI.WebControls;
+
+using System.Data.SqlClient;
+
+using System.Data;
+
+namespace Asp_Assignment
+{
+    public partial class User : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+             try
+
+        {
+
+            username.Focus();
+
+            if (!IsPostBack)
+
+            {
+
+                FillGrid();
+                GetRoleSelect();
+
+            }
+
+        }
+
+        catch
+
+        {
+
+ 
+
+        }
+        }
+        void FillGrid()
+
+        {
+
+            try
+
+            {
+
+                SqlCommand cmd = new SqlCommand();
+
+                cmd.CommandText = "select u_id,u_username,email,password,r.ur_name from tbl_users u inner join tbl_userrole r on u.role_id = r.ur_id  where isActive = 1 and IsDelete = 0";
+
+                cmd.Connection = con;
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+                DataSet ds = new DataSet();
+
+                da.Fill(ds);
+
+                gvDepartments.DataSource = ds;
+
+                gvDepartments.DataBind();
+
+            }
+
+            catch
+
+            {
+
+
+
+            }
+
+        }
+
+        void GetRoleSelect()
+
+        {
+
+            try
+
+            {
+
+                SqlCommand cmd = new SqlCommand();
+
+                cmd.CommandText = "select *from tbl_userrole";
+
+                cmd.Connection = con;
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+               
+                DataSet ds = new DataSet();
+                da.Fill(ds);  // fill dataset
+                role.DataTextField = ds.Tables[0].Columns["ur_name"].ToString(); // text field name of table dispalyed in dropdown
+                role.DataValueField = ds.Tables[0].Columns["ur_id"].ToString();             // to retrive specific  textfield name 
+                role.DataSource = ds.Tables[0];      //assigning datasource to the dropdownlist
+                role.DataBind();  //binding dropdownlist
+
+            }
+
+            catch
+
+            {
+
+
+
+            }
+
+        }
+
+
+
+        void ClearControls()
+
+        {
+
+            try
+
+            {
+
+                username.Text = "";
+
+                email.Text = "";
+
+                password.Text = "";
+
+                hidUserID.Value = "";
+
+                btnSave.Visible = true;
+
+                btnUpdate.Visible = false;
+
+            }
+
+            catch
+
+            {
+
+
+
+                throw;
+
+            }
+
+        }
+        protected void btnSave_Click(object sender, EventArgs e)
+
+        {
+
+            try
+
+            {
+
+                SqlCommand cmd = new SqlCommand();
+
+                cmd.CommandText = "insert into tbl_users (u_username,email,password,role_id) values (@u_username,@email,@password,@role)";
+
+                cmd.Parameters.AddWithValue("@u_username", username.Text);
+
+                cmd.Parameters.AddWithValue("@email", email.Text);
+
+                cmd.Parameters.AddWithValue("@password", password.Text);
+                cmd.Parameters.AddWithValue("@role", role.Text);
+
+                cmd.Connection = con;
+
+                con.Open();
+
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+
+                FillGrid();
+
+                ClearControls();
+
+                lblMessage.Text = "Saved Successfully.";
+
+            }
+
+            catch
+
+            {
+
+
+
+            }
+
+            finally
+
+            {
+
+                if (con.State == ConnectionState.Open)
+
+                    con.Close();
+
+            }
+
+        }
+
+
+
+        protected void btnClear_Click(object sender, EventArgs e)
+
+        {
+
+            try
+
+            {
+
+                ClearControls();
+
+            }
+
+            catch
+
+            {
+
+
+
+            }
+
+        }
+
+
+
+        protected void btnEdit_Click(object sender, EventArgs e)
+
+        {
+
+            try
+
+            {
+
+                ClearControls();
+
+                Button btn = sender as Button;
+
+                GridViewRow grow = btn.NamingContainer as GridViewRow;
+
+                hidUserID.Value = (grow.FindControl("lblUserID") as Label).Text;
+
+                username.Text = (grow.FindControl("lblUsername") as Label).Text;
+
+                email.Text = (grow.FindControl("lblEmail") as Label).Text;
+
+                password.Text = (grow.FindControl("lblPassword") as Label).Text;
+
+                btnSave.Visible = false;
+
+                btnUpdate.Visible = true;
+
+            }
+
+            catch
+
+            {
+
+
+
+            }
+
+        }
+
+
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+
+        {
+
+            try
+
+            {
+
+                SqlCommand cmd = new SqlCommand();
+
+                cmd.CommandText = "update tbl_users set u_username=@u_username,email=@email,password=@password where u_id=@UserID";
+
+                cmd.Parameters.AddWithValue("@u_username", username.Text);
+
+                cmd.Parameters.AddWithValue("@email", email.Text);
+
+                cmd.Parameters.AddWithValue("@password", password.Text);
+
+                cmd.Parameters.AddWithValue("@UserID", hidUserID.Value);
+
+                cmd.Connection = con;
+
+                con.Open();
+
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+
+                FillGrid();
+
+                ClearControls();
+
+                lblMessage.Text = "Updated Successfully.";
+
+            }
+
+            catch
+
+            {
+
+
+
+            }
+
+            finally
+
+            {
+
+                if (con.State == ConnectionState.Open)
+
+                    con.Close();
+
+            }
+
+        }
+
+
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+
+        {
+
+            try
+
+            {
+
+                ClearControls();
+
+                Button btn = sender as Button;
+
+                GridViewRow grow = btn.NamingContainer as GridViewRow;
+
+                SqlCommand cmd = new SqlCommand();
+
+                cmd.CommandText = "update tbl_users set isDelete=1 where u_id=@UserID";
+
+                cmd.Parameters.AddWithValue("@UserID", (grow.FindControl("lblUserID") as Label).Text);
+
+                cmd.Connection = con;
+
+                con.Open();
+
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+
+                FillGrid();
+
+                lblMessage.Text = "Deleted Successfully.";
+
+            }
+
+            catch
+
+            {
+
+
+
+            }
+
+            finally
+
+            {
+
+                if (con.State == ConnectionState.Open)
+
+                    con.Close();
+
+            }
+
+        }
+
+        protected void gvDepartments_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void password_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
